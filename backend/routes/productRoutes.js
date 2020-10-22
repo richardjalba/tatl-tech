c2c0b1ab1061058f6a9abf8f3632e13fc7b057ca
Richard <richardjalba@gmail.com>
2020-10-22 13:48:53 -0700

    Added back end user authentication

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,41 +1,15 @@
 import express from 'express';
-import asyncHandler from 'express-async-handler';
 const router = express.Router();
-import Product from '../models/productModel.js';
+import {
+  getProductById,
+  getProducts,
+} from '../controllers/productController.js';
 
 // express-async-handler middleware
 // i don't wanna put a try-catch in every route
 // it handles exceptions in async express routes & passing to express error handlers
 
-// desc     Fetch all products
-// route    GET  /api/products
-// access   Public
-router.get(
-  '/',
-  asyncHandler(async (req, res) => {
-    const products = await Product.find({});
-
-    res.json(products);
-  })
-);
-
-// desc     Fetch single product
-// route    GET  /api/products/:id
-// access   Public
-router.get(
-  '/:id',
-  asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
-
-    if (product) {
-      res.json(product);
-    } else {
-      res.status(404);
-      throw new Error('Product not found');
-    }
-
-    res.json(product);
-  })
-);
+router.route('/').get(getProducts);
+router.route('/:id').get(getProductById);
 
 export default router;
